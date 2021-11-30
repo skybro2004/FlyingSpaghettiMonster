02fc2ac643c40080d269025bd557f24e964dac4a
skybro2004 <skybro2004@gmail.com>
2021-11-30 17:59:48 +0000

    ㅁㄴㅇㄹ

--- js/tour.js
+++ js/tour.js
@@ -1,3 +1,40 @@
+var pageIndex = 0
+
+
+
 function nextPage(){
-    document.getElementById("iframe").src = "https://skybro2004.com"
+    try{
+        document.getElementsByClassName("node")[pageIndex].classList.remove("current")
+        
+        pageIndex += 1
+    
+        let url = document.getElementsByClassName("node")[pageIndex].getAttribute("src")
+        document.getElementById("iframe").src = url
+        document.getElementsByClassName("node")[pageIndex].classList.add("current")
+    }
+    catch{
+        pageIndex -= 1
+        document.getElementsByClassName("node")[pageIndex].classList.add("current")
+    }
 }
+
+function prevPage(){
+    try{
+        document.getElementsByClassName("node")[pageIndex].classList.remove("current")
+        
+        pageIndex -= 1
+    
+        let url = document.getElementsByClassName("node")[pageIndex].getAttribute("src")
+        document.getElementById("iframe").src = url
+        document.getElementsByClassName("node")[pageIndex].classList.add("current")
+    }
+    catch(error){
+        pageIndex += 1
+        document.getElementsByClassName("node")[pageIndex].classList.add("current")
+    }
+}
+
+
+
+
+
